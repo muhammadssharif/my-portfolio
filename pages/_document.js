cceb29be59bab3d5e922f0c6bcee823bf6866223
muhammadssharif <msharif28@myseneca.ca>
2023-10-01 15:13:18 -0400

    googlefonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=optional"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Text&display=optional"
+        rel="stylesheet"
+      />
+       <link
+        href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=optional"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
